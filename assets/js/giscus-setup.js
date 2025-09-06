67e7588c268bfca3a52c6215ae13e469ec4e50f1
Hcy-43 <101809005+Hcy-43@users.noreply.github.com>
2025-09-06 10:19:58 +0000

    Deploying to gh-pages from @ Hcy-43/Hcy-43.github.io@df38bf8c8f4f62d906d34ea8a53b833f1ea19650 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Hcy-43/al-folio",
+    "data-repo": "Hcy-43/Hcy-43.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
